3fc2c2a95b941b1ee5399dcedd94dc440850ab5d
tembalii <159453738+tembalii@users.noreply.github.com>
2024-10-01 07:22:00 +0100

    Update SampleCode.java

--- src/main/java/SampleCode.java
+++ src/main/java/SampleCode.java
@@ -1,13 +1,18 @@
-public class SampleCode {
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class SimpleExample {
     public static void main(String[] args) {
         ObjectMapper mapper = new ObjectMapper();
-        ObjectNode node = mapper.createObjectNode();
+        ObjectNode node = mapper.createObjectNode();  // Creating a JSON object
 
         try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("data.ser"))) {
             // This line should be flagged by the rule
-            Object deserializedObject = objectInputStream.readObject();
-
-            System.out.println("Deserialized object: " + deserializedObject);
+            Object obj = objectInputStream.readObject();
+            System.out.println("Object read: " + obj);
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
