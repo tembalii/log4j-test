0e4d3994b6810b537708d6ba8e45ec53ae2fc5d3
tembalii <159453738+tembalii@users.noreply.github.com>
2024-10-01 07:21:06 +0100

    Update HelloLog.java

--- src/main/java/HelloLog.java
+++ src/main/java/HelloLog.java
@@ -55,20 +55,5 @@ public class HelloLog {
         } catch (JWTCreationException exception){
             //Invalid Signing configuration / Couldn't convert Claims.
         }
-     
-    public static void main(String[] args) {
-        ObjectMapper mapper = new ObjectMapper();
-        ObjectNode node = mapper.createObjectNode();
-
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("data.ser"))) {
-            // This line should be flagged by the rule
-            Object deserializedObject = objectInputStream.readObject();
-
-            System.out.println("Deserialized object: " + deserializedObject);
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-     
     }
 }
